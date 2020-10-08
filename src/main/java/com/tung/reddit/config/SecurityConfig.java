4ecef6637ac4e1ce1c963959433e5042ce46912f
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-10-08 09:05:20 +0700

    dung commit

--- src/main/java/com/tung/reddit/config/SecurityConfig.java
+++ src/main/java/com/tung/reddit/config/SecurityConfig.java
@@ -1,7 +1,48 @@
 package com.tung.reddit.config;
 
+import com.tung.reddit.services.AppUserServiceD;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AppUserServiceD appUserServiceD;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService((UserDetailsService) appUserServiceD)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/","/home","/create-account","/post/detail/*").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/account/**").hasAnyRole("USER","ADMIN")
+                .antMatchers("/post/**").hasAnyRole("USER","ADMIN")
+                .antMatchers("/**").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/")
+                .and().exceptionHandling()
+                .accessDeniedPage("/Access_Denied")
+        ;
+        http.csrf().disable();
+    }
 
 }
--- src/main/java/com/tung/reddit/services/AppRoleServiceD.java
+++ src/main/java/com/tung/reddit/services/AppRoleServiceD.java
@@ -1,4 +1,20 @@
 package com.tung.reddit.services;
 
+import com.tung.reddit.models.AppRole;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
+
 public interface AppRoleServiceD {
+    Iterable<AppRole> getAllRole();
+
+    AppRole getRoleById(Long id);
+
+    AppRole save(AppRole appRole);
+
+    void remove(AppRole appRole);
+
+    AppRole getRoleByName(String roleName);
 }
