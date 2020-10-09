59ea08946a2cc0c294720e315f42737e2bb4ca9e
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-10-09 14:49:01 +0700

    dung commit

--- src/main/java/com/tung/reddit/SecurityConfig.java
+++ src/main/java/com/tung/reddit/SecurityConfig.java
@@ -1,50 +1,50 @@
-package com.tung.reddit;
-
-import com.tung.reddit.services.AppUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Qualifier("appUserServiceImpl")
-    @Autowired
-    AppUserService appUserServiceImpl;
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService((UserDetailsService) appUserServiceImpl).passwordEncoder(NoOpPasswordEncoder.getInstance());
-    }
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/").permitAll()
-                .and()
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.POST,"/create").permitAll()
+//package com.tung.reddit;
+//
+//import com.tung.reddit.services.AppUserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Qualifier("appUserServiceImpl")
+//    @Autowired
+//    AppUserService appUserServiceImpl;
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService((UserDetailsService) appUserServiceImpl).passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+//
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers(HttpMethod.GET,"/").permitAll()
 //                .and()
-//                .authorizeRequests().antMatchers("/user**").hasRole("USER")
+////                .authorizeRequests()
+////                .antMatchers(HttpMethod.POST,"/create").permitAll()
+////                .and()
+////                .authorizeRequests().antMatchers("/user**").hasRole("USER")
+////                .and()
+////                .authorizeRequests().antMatchers("/premium**").hasRole("PREMIUM_USER")
+////                .and()
+////                .authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+////                .and()
+////                .authorizeRequests().antMatchers("/admin**").hasRole("MODERATOR")
+////                .and()
+//                .formLogin().loginPage("/login")
 //                .and()
-//                .authorizeRequests().antMatchers("/premium**").hasRole("PREMIUM_USER")
-//                .and()
-//                .authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
-//                .and()
-//                .authorizeRequests().antMatchers("/admin**").hasRole("MODERATOR")
-//                .and()
-                .formLogin().loginPage("/login")
-                .and()
-                .logout().logoutSuccessUrl("/").and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-        http.csrf().disable();
-    }
-
-}
+//                .logout().logoutSuccessUrl("/").and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+//        http.csrf().disable();
+//    }
+//
+//}
